c7d1f4168bfa5caddbc79f25198e09f61ae57f87
TecDoGt <ddelaguila@tecdogt.com>
2016-11-13 23:19:10 -0600

    first commit

---
+++ www/js/principal.js
@@ -0,0 +1,23 @@
+function suma(){ //suma
+    a = document.getElementById("edit1").value*1;
+    b = document.getElementById("edit2").value*1;
+    alert(a + b);
+}
+
+function resta(){ //resta
+    a = document.getElementById("edit1").value*1;
+    b = document.getElementById("edit2").value*1;
+    alert(a - b);
+}
+
+function mult(){ //multiplicacion
+    a = document.getElementById("edit1").value*1;
+    b = document.getElementById("edit2").value*1;
+    alert(a * b);
+}
+
+function di(){ //division
+    a = document.getElementById("edit1").value*1;
+    b = document.getElementById("edit2").value*1;
+    alert(a/b);
+}
